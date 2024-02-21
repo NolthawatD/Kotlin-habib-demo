2329dd15473fffbdd8e9fa075a9ad5aacbd2505d
Nolthawat Deangharn <nolthawat.d@gmail.com>
2024-02-21 15:40:49 +0700

    Add new Task controller

--- src/main/kotlin/com/example/habibidemo/service/TaskService.kt
+++ src/main/kotlin/com/example/habibidemo/service/TaskService.kt
@@ -3,10 +3,15 @@ package com.example.habibidemo.service
 import com.example.habibidemo.data.Task
 import com.example.habibidemo.data.model.TaskCreateRequest
 import com.example.habibidemo.data.model.TaskDto
+import com.example.habibidemo.data.model.TaskUpdateRequest
+import com.example.habibidemo.exception.BadRequestException
 import com.example.habibidemo.exception.TaskNotFoundException
 import com.example.habibidemo.repository.TaskRepository
 import org.springframework.stereotype.Service
+import org.springframework.util.ReflectionUtils
+import java.lang.reflect.Field
 import java.util.stream.Collectors
+import kotlin.reflect.full.memberProperties
 
 
 @Service
@@ -48,5 +53,39 @@ class TaskService(private val repository: TaskRepository) {
 
     fun getAllClosedTasks(): List<TaskDto> =
         repository.queryAllClosedTasks().stream().map(this::mappingEntityToDto).collect(Collectors.toList())
+
+    fun createTask(request: TaskCreateRequest): TaskDto {
+        if (repository.doesDescriptionExist(request.description)) {
+            throw BadRequestException("There is already a task with the description: ${request.description}")
+        }
+        val task = Task()
+        mappingFromRequestToEntity(task, request)
+        val savedTask: Task = repository.save(task)
+        return mappingEntityToDto(savedTask)
+    }
+
+    fun updateTask(id: Long, request: TaskUpdateRequest): TaskDto {
+        checkTaskForId(id)
+        val existingTask: Task = repository.findTaskById(id)
+
+        for (prop in TaskUpdateRequest::class.memberProperties) {
+            if (prop.get(request) != null) {
+                val field: Field? = ReflectionUtils.findField(Task::class.java, prop.name)
+                field?.let {
+                    it.isAccessible = true
+                    ReflectionUtils.setField(it, existingTask, prop.get(request))
+                }
+            }
+        }
+
+        val savedTask: Task = repository.save(existingTask)
+        return mappingEntityToDto(savedTask)
+    }
+
+    fun deleteTask(id: Long): String {
+        checkTaskForId(id)
+        repository.deleteById(id)
+        return "Task with the ID: $id has been deleted."
+    }
 
 }
