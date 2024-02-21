f2001cce6cc1af12eff5c057c34311bf80639e30
Nolthawat Deangharn <nolthawat.d@gmail.com>
2024-02-21 11:51:43 +0700

    initialize

---
+++ src/main/kotlin/com/example/habibidemo/HabibiDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.habibidemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HabibiDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<HabibiDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/habibidemo/HabibiDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.habibidemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HabibiDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
